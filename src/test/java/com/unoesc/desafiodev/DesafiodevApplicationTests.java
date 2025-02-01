a4070e1c196df4450b424e325154d96b8ea789ae
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-02-01 18:10:01 -0300

    Adicionando novo projeto Spring

---
+++ src/test/java/com/unoesc/desafiodev/DesafiodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unoesc.desafiodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
