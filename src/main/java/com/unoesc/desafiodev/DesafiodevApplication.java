57287c715ac37747d088d8f1cae50f4bd29a9911
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-02-02 15:36:14 -0300

    Referenciando a classe principal

--- src/main/java/com/unoesc/desafiodev/DesafiodevApplication.java
+++ src/main/java/com/unoesc/desafiodev/DesafiodevApplication.java
@@ -1,13 +1,20 @@
 package com.unoesc.desafiodev;
 
+import com.unoesc.desafiodev.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafiodevApplication {
+public class DesafiodevApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafiodevApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args){
+		Principal principal = new Principal();
+		principal.executar();
+	}
 }
