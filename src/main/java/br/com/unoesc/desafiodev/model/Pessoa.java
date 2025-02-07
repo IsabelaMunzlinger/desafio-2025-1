1a3014be9b2c784fe4a2df03ed6c8e312cd08b2d
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-02-07 18:53:05 -0300

    Atualizando a classe pessoa

--- src/main/java/br/com/unoesc/desafiodev/model/Pessoa.java
+++ src/main/java/br/com/unoesc/desafiodev/model/Pessoa.java
@@ -1,111 +1,62 @@
 package br.com.unoesc.desafiodev.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Table(name = "pessoa", schema = "desafio")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Pessoa {
-    @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column
+    @Id
     private Integer id;
 
-    @Column(name = "cpf", nullable = false, length = 14)
-    private String cpf;
+    @Transient  // Esse campo será preenchido, mas não será salvo no banco
+    private Name name;
 
-    @Column(name = "ativo", nullable = false)
-    private Boolean ativo = false;
+    @Column
+    private String nome; // Esse é o campo real que vai ser armazenado
 
-    @Column(name = "nome", nullable = false, length = 100)
-    private String nome;
-
-    @Column(name = "email", nullable = false, length = 100)
+    @Column
     private String email;
 
-    @Column(name = "telefone", length = 20)
-    private String telefone;
+    //@Column(nullable = false, unique = true)
+    @NonNull
+    private String cpf;
 
-    @Column(name = "usuario")
-    private String usuario;
-
-    @Column(name = "senha")
+    @Column(nullable = false)
     private String senha;
 
-    public Pessoa() {
-    }
+    @Column
+    private boolean ativo;
 
+    @JsonIgnore
+    @Column(nullable = false)
+    private String usuario;
+
+
+    /*
+    Atributos vindos da API
+     */
+
+
+    @JsonProperty("phone") // Mapeia o campo JSON "phone" para a variável "telefone"
+    @Column(nullable = false)
+    private String telefone;
+
+    // Método que extrai os nomes do objeto "name"
     @JsonProperty("name")
-    private void unpackName(Name name) {
+    public void unpackName(Name name) {
         this.nome = name.getFirst() + " " + name.getLast();
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Boolean getAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(Boolean ativo) {
-        this.ativo = ativo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
+    // Classe interna para mapear o objeto "name"
     @JsonIgnoreProperties(ignoreUnknown = true)
     private static class Name {
         private String first;
@@ -119,4 +70,160 @@ public class Pessoa {
             return last;
         }
     }
+
+    @JsonIgnore
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    @JsonIgnore
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //@JsonIgnore
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @NonNull
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(@NonNull String cpf) {
+        this.cpf = cpf;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+    //    /*
+//    Getters e setters
+//    */
+//    public void setCpf(String s) {
+//        this.cpf = s;
+//    }
+//
+//    public void setAtivo(boolean s) {
+//    }
+//
+//    public void setUsuario(String s) {
+//    }
+//
+//    public String getPhone() {
+//        return telefone;
+//    }
+//
+//    public String getCpf() {
+//        return cpf;
+//    }
+//
+//    public String getUsuario() {
+//        return getUsuario();
+//    }
+//
+//    public void setSenha(String s) {
+//        this.senha = s;
+//    }
+//
+//    public Object getSenha() {
+//        return senha;
+//    }
+//
+//
+//    // Getters e Setters
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//
+//    public boolean isAtivo() {
+//        return true;
+//    }
+//
+//
+//    @JsonIgnore
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//
+//
+//
+//
+//    @JsonIgnore
+//    public String getTelefone() {
+//        return telefone;
+//    }
+//
+//    public void setTelefone(String telefone) {
+//        this.telefone = telefone;
+//    }
 }
