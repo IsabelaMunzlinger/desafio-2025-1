c44188432902bd266324520dc9c4bd428b94b258
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-02-10 00:44:31 -0300

    Atualizando imports

--- src/main/java/br/com/unoesc/desafiodev/DesafiodevApplication.java
+++ src/main/java/br/com/unoesc/desafiodev/DesafiodevApplication.java
@@ -1,27 +1,30 @@
-package com.unoesc.desafiodev;
+package br.com.unoesc.desafiodev;
 
-import com.unoesc.desafiodev.principal.Principal;
-import com.unoesc.desafiodev.telas.TelaLogin;
+import br.com.unoesc.desafiodev.model.Pessoa;
+import br.com.unoesc.desafiodev.principal.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.swing.*;
-
 @SpringBootApplication
 public class DesafiodevApplication implements CommandLineRunner {
+
+	@Autowired
+	private Principal principal;
+
+	Pessoa pessoa = new Pessoa();
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafiodevApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args){
-		SwingUtilities.invokeLater(() -> {
-			new TelaLogin().setVisible(true);
-		});
 
-//		Principal principal = new Principal();
-//		principal.executar();
+	@Override
+	public void run(String... args) {
+		System.out.println("Aplicação iniciada! Banco de dados conectado com sucesso.");
+		principal.login();
+		principal.exibirMenu(pessoa);
 	}
 }
+
