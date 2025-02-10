de89d1415df910f6ba0a1ef80e58cc2f6922ee6c
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-02-10 00:59:28 -0300

    Adicionando solicitação de endereço

--- src/main/java/br/com/unoesc/desafiodev/service/PessoaEnderecoService.java
+++ src/main/java/br/com/unoesc/desafiodev/service/PessoaEnderecoService.java
@@ -1,21 +1,41 @@
 package br.com.unoesc.desafiodev.service;
 
-import br.com.unoesc.desafiodev.model.Pessoaendereco;
+import br.com.unoesc.desafiodev.model.Pessoa;
+import br.com.unoesc.desafiodev.model.PessoaEndereco;
 import br.com.unoesc.desafiodev.repository.PessoaEnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Scanner;
+
 @Service
 public class PessoaEnderecoService {
 
-    private final PessoaEnderecoRepository pessoaEnderecoRepository;
-
     @Autowired
-    public PessoaEnderecoService(PessoaEnderecoRepository pessoaEnderecoRepository) {
-        this.pessoaEnderecoRepository = pessoaEnderecoRepository;
-    }
+    private PessoaEnderecoRepository pessoaEnderecoRepository;
 
-    public Pessoaendereco salvarEndereco(Pessoaendereco endereco) {
-        return pessoaEnderecoRepository.save(endereco);
+    private final Scanner leitura = new Scanner(System.in);
+
+    // Solicita os dados de endereço de uma pessoa
+    public void solicitarEndereco(Pessoa pessoa) {
+        PessoaEndereco endereco = new PessoaEndereco();
+
+        System.out.print("Cidade: ");
+        endereco.setCidade(leitura.nextLine());
+
+        System.out.print("CEP: ");
+        endereco.setCep(leitura.nextLine());
+
+        System.out.print("Rua: ");
+        endereco.setRua(leitura.nextLine());
+
+        System.out.print("Número: ");
+        endereco.setNumero(leitura.nextInt());
+        leitura.nextLine(); // Consumir a quebra de linha
+
+        endereco.setPessoa(pessoa);
+        pessoa.setPessoaEndereco(endereco);
+
+        pessoaEnderecoRepository.save(endereco); // Salva no banco
     }
 }
