4faea3d23ac300149fad6e458512a8030f98e034
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2025-02-10 01:11:15 -0300

    Inteface genérica usada para conversão

--- src/main/java/br/com/unoesc/desafiodev/service/IConverteDados.java
+++ src/main/java/br/com/unoesc/desafiodev/service/IConverteDados.java
@@ -1,5 +1,7 @@
 package br.com.unoesc.desafiodev.service;
 
+
+//Definição genérica para usar poder implementar em qualquer classe
 public interface IConverteDados {
     <T> T obterDados(String json, Class<T> classe);
 }
